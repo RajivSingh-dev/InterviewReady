dccafb5b3edd57109488be7d18d1e472aa9fb880
Rajiv Singh <rajiv.singh@aas.services>
2022-07-25 18:18:03 +0530

    Added modal popup

---
+++ InterviewReady/Controllers/InterviewExController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InterviewReady.Controllers
+{
+    public class InterviewExController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
