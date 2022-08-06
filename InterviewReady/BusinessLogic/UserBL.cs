41a8a74cb36e813a42ed7eca60885ba93148db63
Rajiv Singh <rajiv.singh@aas.services>
2022-08-06 14:57:27 +0530

    update

---
+++ InterviewReady/BusinessLogic/UserBL.cs
@@ -0,0 +1,31 @@
+﻿using InterviewReady.UserContext;
+
+namespace InterviewReady.BusinessLogic
+{
+    public class UserBL
+    {
+        private UserDbContext userDbContext;
+         
+        public UserBL(UserDbContext userDbContext)
+        {
+            this.userDbContext = userDbContext;
+        }
+
+        public dynamic UserExits(UserShoppingTable model)
+        {
+            bool rValue = false;
+
+            List<UserShoppingTable> students;
+
+            var username = model.Username;
+            rValue =  userDbContext.UserShoppingTable.Any(r => r.Username == username);
+
+            
+
+
+            return rValue;
+        }
+    }
+}
+
+
---
+++ InterviewReady/Controllers/UserDataApiController.cs
@@ -0,0 +1,28 @@
+﻿using InterviewReady.BusinessLogic;
+using InterviewReady.UserContext;
+using Microsoft.AspNetCore.Mvc;
+namespace InterviewReady.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserDataApiController : Controller
+    {
+        public readonly UserDbContext userontext;
+        public UserDataApiController(UserDbContext userontext)
+        {
+            this.userontext = userontext;
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult Get()
+        {
+
+            UserBL userBL = new UserBL(userontext);
+            userBL.UserExists()
+            return View();
+        }
+
+
+    }
+}
